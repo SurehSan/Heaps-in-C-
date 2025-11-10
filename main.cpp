1b174e37a74f6823bac484eb75ac78d5139c3de4
Sureh San <140090587+SurehSan@users.noreply.github.com>
2025-11-09 21:52:54 -0600

    Implement main function for heap operations

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+#include "maxheap25.h"
+
+int main(int argc, char *argv[])
+{
+    heap h;
+    int rv, x;
+
+    ifstream fin(argv[1]);
+    int cmd;
+
+
+    while (fin >> cmd)
+    {
+        if (cmd == 0)
+            break;
+        if (cmd == 1)
+        {
+            fin >> x;
+            h.add(x);
+        }
+        else if (cmd == -1)
+        {
+            rv = h.remove();
+            cout << "Removed " << rv << "." << endl;
+        }
+        else if (cmd == 3)
+        {
+            h.printA();
+        }
+
+    }
+
+    return 0;
+}
